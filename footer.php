0f0f9aaa6bc4933dc5e1c8718128138ff86a0c66
Erick Wright <erickwright@Ericks-MacBook-Pro.local>
2013-08-03 23:11:30 -0400

    local repo for github

---
+++ footer.php
@@ -0,0 +1,15 @@
+    <hr>
+
+      <footer>
+        <p>© Company 2012</p>
+      </footer>
+
+    </div> <!-- /container -->
+
+    <!-- Le javascript
+    ================================================== -->
+    <script src="../asset/js/jquery.js"></script>
+    <script src="../asset/js/bootstrap.js"></script>
+
+  </body>
+</html>
